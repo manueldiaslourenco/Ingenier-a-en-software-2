7e8a474a5041047877b07c6d1bf2574a3e3540c4
Manuel Dias Lourenco <manueldiasuala@gmail.com>
2024-05-02 19:49:24 -0300

    agrego y fixeo problemas de redireccion.

--- Proyecto-ing2/administrador/views.py
+++ Proyecto-ing2/administrador/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from usuarios.models import Usuario
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-# Create your views here.
-
+@login_required(login_url=reverse_lazy('home'))
 def index(request):
+    usuario_actual = request.user
+    if not usuario_actual.is_superuser:
+        return redirect('home')
     return render(request, 'indexadmin.html')
 
 def calcular_edad(fecha_nacimiento):
@@ -12,7 +16,12 @@ def calcular_edad(fecha_nacimiento):
     edad = fecha_actual.year - fecha_nacimiento.year - ((fecha_actual.month, fecha_actual.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
     return edad
 
+@login_required(login_url=reverse_lazy('home'))
 def usuarios(request):
+    usuario_actual = request.user
+    if not usuario_actual.is_superuser:
+        return redirect('home')
+    
     usuarios = Usuario.objects.all()
     for usuario in usuarios:
         usuario.edad = calcular_edad(usuario.fecha_nacimiento)
--- Proyecto-ing2/usuarios/views.py
+++ Proyecto-ing2/usuarios/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import HttpResponse
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-
+@login_required(login_url=reverse_lazy('home'))
 def ver_perfil(request, id):
-    # Supongamos que 'lista' es tu lista de usuarios.
+    usuario_actual = request.user
     UserModel = get_user_model()
     try:
         usuario= UserModel.objects.get(pk=id)
-        if usuario.is_superuser:
+        if usuario.is_superuser and usuario_actual.is_superuser:
             return redirect('admin')
+        elif usuario.is_superuser:
+            return HttpResponse("Este elemento no existe.")
         else:
             return render(request, 'profile.html', {'param': usuario})
+        
     except UserModel.DoesNotExist:
         return HttpResponse("Este elemento no existe.")
