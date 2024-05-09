29725723ab0c4377e75a06441276659f60b6ea04
agosrubbini <agostinarubbini@gmail.com>
2024-05-09 20:30:19 -0300

    modifique las urls del admin

--- Proyecto-ing2/administrador/urls.py
+++ Proyecto-ing2/administrador/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('administracion/', views.index, name="admin"),
-    path('administracion/usuarios/', views.usuarios, name="usuarios"),
+    path('administracion/usuarios/', views.usuarios, name="lista usuarios"),
     path('administracion/crear_usuario/', views.cuestionario_crear_usuario, name="crear usuario"),
-    path('administracion/empleados/', views.empleados, name="empleados"),
+    path('administracion/empleados/', views.empleados, name="lista empleados"),
     path('administracion/crear_empleado/', views.cuestionario_crear_empleado, name="crear empleado"),
     path('administracion/usuarios/bloquear_usuario', views.bloquear_usuario, name="bloquear usuario"),
     path('administracion/usuarios/desbloquear_usuario', views.desbloquear_usuario, name="desbloquear usuario"),
