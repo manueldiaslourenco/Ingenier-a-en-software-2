37bb146c2fd3f25279d176056749484cb2a31c1e
Joakulo <joacosanta@gmail.com>
2024-05-09 22:43:35 -0300

    Added navbar lateral y sede en navbar empleados

--- Proyecto-ing2/empleados/views.py
+++ Proyecto-ing2/empleados/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from .backend import  chequear_empleado
+from empleados.models import EmpleConSede, Sede
 
 @login_required(login_url=reverse_lazy('home'))
 def index(request):
     chequear_empleado(request.user)
     
-    return render(request, 'employees_index.html')
+    id = request.user.id
+    emple_sede = EmpleConSede.objects.get(user_id=id)
+    sede = Sede.objects.get(id=emple_sede.sede_id)
+
+    return render(request, 'employees_index.html', { 'nombre_sede' : sede.nombre})
 
--- Proyecto-ing2/usuarios/views.py
+++ Proyecto-ing2/usuarios/views.py
@@ -13,6 +13,8 @@ def ver_perfil(request, id):
         usuario= UserModel.objects.get(pk=id)
         if usuario.is_superuser and usuario_actual.is_superuser:
             return redirect('admin')
+        elif usuario.is_staff and usuario_actual.is_staff:
+            return redirect('panel empleados')
         elif usuario.is_superuser:
             return HttpResponse("Este elemento no existe.")
         else:
