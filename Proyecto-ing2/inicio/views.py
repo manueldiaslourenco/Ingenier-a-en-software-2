bc902d0dda1c27df883a7c449a8522a552a156a7
Joaquín Santa Cruz <joacosanta@gmail.com>
2024-05-13 12:37:19 -0300

    Finished Subir publicacion

--- Proyecto-ing2/inicio/views.py
+++ Proyecto-ing2/inicio/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from publicaciones.models import Publicacion
+from embarcaciones.models import ImagenEmbarcacion
 
 def redirigir_inicio(request):
     return redirect('home')
 
 def inicio(request):
     publicaciones = Publicacion.objects.all()
+    for publicacion in publicaciones:
+        post_image = ImagenEmbarcacion.objects.get(embarcacion=publicacion.embarcacion.id)
+        publicacion.imagen = post_image.nombre_especifico
     return render(request, 'index.html', {'publicaciones' : publicaciones})
--- Proyecto-ing2/publicaciones/views.py
+++ Proyecto-ing2/publicaciones/views.py
@@ -7,22 +7,30 @@ from .backend import cargar_publicacion_back
 
 @login_required(login_url=reverse_lazy('iniciar sesion'))
 def cuestionario_cargar_publicacion(request):
-    ok=False
-    form = formularioCrearPublicacion()
     usuario_actual = request.user
-    embarcaciones = Embarcacion.objects.filter(dueño=usuario_actual)
-    matriculas_embarcaciones = list(embarcaciones.values_list('matricula', flat=True))
+    embarcaciones_usuario = Embarcacion.objects.filter(dueño=usuario_actual)
+    tiene_embarcaciones = embarcaciones_usuario.exists()
     
-    if request.method == 'POST':
-        form = formularioCrearPublicacion(request.POST)
-        ok= False
-        if form.is_valid():
-            usuario_actual_mail = request.user.mail
-            lista= []
-            lista.append(usuario_actual_mail)
-            lista.append(form.cleaned_data['embarcacion'])
-            lista.append(form.cleaned_data['monto'])
-            lista.append(form.cleaned_data['descripcion'])
-            ok = cargar_publicacion_back(lista, form)
+    if tiene_embarcaciones:
+        ok=False
+        form = formularioCrearPublicacion()
+        usuario_actual = request.user
+        embarcaciones = Embarcacion.objects.filter(dueño=usuario_actual)
+        matriculas_embarcaciones = list(embarcaciones.values_list('matricula', flat=True))
+        
+        if request.method == 'POST':
+            form = formularioCrearPublicacion(request.POST)
+            ok= False
+            if form.is_valid():
+                usuario_actual_mail = request.user.mail
+                lista= []
+                lista.append(usuario_actual_mail)
+                lista.append(form.cleaned_data['embarcacion'])
+                lista.append(form.cleaned_data['monto'])
+                lista.append(form.cleaned_data['descripcion'])
+                ok = cargar_publicacion_back(lista, form)
 
-    return render(request, 'register_post.html', {'form': form, 'ok': ok, 'embarcaciones':  matriculas_embarcaciones})
+        return render(request, 'register_post.html', {'form': form, 'ok': ok, 'embarcaciones':  matriculas_embarcaciones})
+    else:
+        mensaje = "Para crear una publicación, primero debes cargar una embarcación desde tu perfil."
+        return render(request, 'index.html', {'mensaje': mensaje})
