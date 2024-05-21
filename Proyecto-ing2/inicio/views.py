773769beb50f748fcdae6a921b50056a7d296ca0
Manuel Dias Lourenco <manueldiasuala@gmail.com>
2024-05-21 16:27:35 -0300

    ver perfil uusario comun casi terminado.

--- Proyecto-ing2/inicio/views.py
+++ Proyecto-ing2/inicio/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from publicaciones.models import Publicacion
 from embarcaciones.models import ImagenEmbarcacion
+from usuarios.models import Usuario
 
 def redirigir_inicio(request):
     return redirect('home')
 
 def inicio(request):
-    publicaciones = Publicacion.objects.all()
+    publicaciones= Publicacion.objects.exclude(autor__is_blocked= True)
     for publicacion in publicaciones:
         post_image = ImagenEmbarcacion.objects.filter(embarcacion=publicacion.embarcacion.id)
         publicacion.imagen = post_image[0].nombre_especifico
