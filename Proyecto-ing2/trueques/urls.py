5cbbbab93764fd1cef1b4ef73d83f180a01b3cf7
SimonMcGovern <simon.mc1070@gmail.com>
2024-06-05 19:12:49 -0300

    Se pushea la funcion anular trueque y se agregan cosas al listar trueques y sus detalles

--- Proyecto-ing2/trueques/urls.py
+++ Proyecto-ing2/trueques/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('ver_detalle_trueque/<int:id_trueque>/', views.ver_trueque, name='ver detalle trueque'),
-    path('completar_trueque/<int:id_trueque>/', views.completar_trueque, name= 'completar trueque')
+    path('completar_trueque/<int:id_trueque>/', views.completar_trueque, name= 'completar trueque'),
+    path('cancelar_trueque/<int:id_trueque>', views.cancelar_trueque, name= 'cancelar trueque' )
 ]
--- Proyecto-ing2/trueques/views.py
+++ Proyecto-ing2/trueques/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from datetime import datetime, timedelta
 from .models import Trueque
+from embarcaciones.models import ImagenEmbarcacion
+from vehiculos.models import ImagenVehiculo
 
 # Create your views here.
 
@@ -10,14 +12,18 @@ from .models import Trueque
 def ver_trueque(request, id_trueque):
 
     trueque= Trueque.objects.get(id= id_trueque)
+    imagenes_publicacion = ImagenEmbarcacion.objects.filter(embarcacion= trueque.embarcacion1.id)
+    if trueque.embarcacion2:
+        imagenes_oferta = ImagenEmbarcacion.objects.filter(embarcacion= trueque.embarcacion2.id)
+    elif trueque.vehiculo:
+        imagenes_oferta = ImagenVehiculo.objects.filter(vehiculo= trueque.vehiculo.id)
     fecha_actual = datetime.now().date()
     
     if (fecha_actual - trueque.fecha_inicio.date()) >= timedelta(days=30):
         mensaje= "Pasaron mas de 30 dias del incio del trueque se recomienda su cancelación."
-        return render(request, 'trueque_detail.html', {'mensaje': mensaje,
-                                                       'trueque': trueque})
+        return render(request, 'trueque_detail.html', {'mensaje': mensaje, 'trueque':trueque, 'imagenes_publicacion': imagenes_publicacion, 'imagenes_oferta':imagenes_oferta})
     else:
-        return render(request, 'trueque_detail.html', {'trueque': trueque})
+        return render(request, 'trueque_detail.html', {'trueque':trueque, 'imagenes_publicacion': imagenes_publicacion, 'imagenes_oferta':imagenes_oferta})
     
 @login_required(login_url=reverse_lazy('iniciar sesion'))
 def completar_trueque(request, id_trueque):
@@ -37,5 +43,13 @@ def completar_trueque(request, id_trueque):
         embarcacion2= trueque.embarcacion2
         embarcacion2.dueño= trueque.usuario1
         embarcacion2.save()
+
+    return redirect('ver detalle trueque', id_trueque)
+
+@login_required(login_url=reverse_lazy('iniciar_sesion'))
+def cancelar_trueque(request, id_trueque):
+    trueque = Trueque.objects.get(id= id_trueque)
+    trueque.estado = 'Cancelado'
+    trueque.save()
 
     return redirect('ver detalle trueque', id_trueque)
