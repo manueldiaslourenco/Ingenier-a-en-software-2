09db0b03b30a82e7795e3762dbe58179a47a42db
manueldiaslourenco <manueldiasuala@gmail.com>
2024-06-05 17:34:59 -0300

    validar trueque

--- Proyecto-ing2/trueques/urls.py
+++ Proyecto-ing2/trueques/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('ver_detalle_trueque/<int:id_trueque>/', views.ver_trueque, name='ver detalle trueque'),
+    path('completar_trueque/<int:id_trueque>/', views.completar_trueque, name= 'completar trueque')
 ]
--- Proyecto-ing2/trueques/views.py
+++ Proyecto-ing2/trueques/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from datetime import datetime, timedelta
@@ -14,6 +14,28 @@ def ver_trueque(request, id_trueque):
     
     if (fecha_actual - trueque.fecha_inicio.date()) >= timedelta(days=30):
         mensaje= "Pasaron mas de 30 dias del incio del trueque se recomienda su cancelación."
-        return render(request, 'trueque_detail.html', {'mensaje': mensaje})
+        return render(request, 'trueque_detail.html', {'mensaje': mensaje,
+                                                       'trueque': trueque})
     else:
-        return render(request, 'trueque_detail.html')
+        return render(request, 'trueque_detail.html', {'trueque': trueque})
+    
+@login_required(login_url=reverse_lazy('iniciar sesion'))
+def completar_trueque(request, id_trueque):
+
+    trueque= Trueque.objects.get(id= id_trueque)
+    trueque.estado= 'Completado'
+    trueque.save()
+
+    embarcacion= trueque.embarcacion1
+    embarcacion.dueño= trueque.usuario2
+    embarcacion.save()
+    if trueque.vehiculo_id:
+        vehiculo= trueque.vehiculo
+        vehiculo.dueño= trueque.usuario1
+        vehiculo.save()
+    else:
+        embarcacion2= trueque.embarcacion2
+        embarcacion2.dueño= trueque.usuario1
+        embarcacion2.save()
+
+    return redirect('ver detalle trueque', id_trueque)
